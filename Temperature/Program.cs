cdc3829a42be5e6d8cfce523b048d128c3cfc784
A'Darius Craig <adariusc03@gmail.com>
2020-09-09 10:37:32 -0500

    TempConverter

---
+++ Temperature/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Temperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var celsius = TempConverter.FahrenheitToCelsius(80); //output 26.7
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(38); // output 100.4
+
+            Console.WriteLine("Results of Converting:");
+            Console.WriteLine();
+            Console.WriteLine($"Celcius {celsius}");
+            Console.WriteLine();
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+        }
+    }
+}
